2a49b26e84a8892dfcaca50cbc08c28dba777668
Gale, Jack <Jack.Gale@tabcorp.com.au>
2025-01-16 15:10:44 +1100

    section 20 complete

---
+++ src/WebApps/ShoppingWeb/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using ShoppingWeb.Models.Catalog;
+global using ShoppingWeb.Models.Ordering;
+global using ShoppingWeb.Models.Basket;
+global using Refit;
+global using ShoppingWeb.Services;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
